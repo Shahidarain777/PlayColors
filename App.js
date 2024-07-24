d002e4296343d44cfe3f89c51645f8b51323236f
ShahidSaffranlabs <shahid.ali@saffranlabs.com>
2024-07-24 21:23:33 +0500

    play with colors task done.

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useMemo, useState } from "react";
+import { Pressable, Text, View, StyleSheet } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [bgColor, setBgColor] = useState("#ffffff");
+
+  const changeColor = () => {
+    const randomInt = Math.floor(Math.random() * 16777215);
+    const hexString = randomInt.toString(16).padStart(6, "0");
+    setBgColor(`#${hexString}`);
+  };
+
+  const ColorBox = useMemo(() => {
+    return (
+      <Pressable
+        onPress={() => {
+          changeColor();
+          console.log("Pressed!");
+        }}
+        style={{
+          ...styles.pressable,
+          backgroundColor: bgColor,
+        }}
+      >
+        <Text style={styles.text}>Hello there; color: {bgColor}</Text>
+      </Pressable>
+    );
+  }, [bgColor]);
+  return <View style={styles.container}>{ColorBox}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  pressable: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    userSelect: "none", // Disable text selection
+    cursor: "pointer",
+  },
+
+  text: {
+    fontSize: 16,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 4,
   },
 });
